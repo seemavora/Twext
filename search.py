07272940a3de44e794d902a9145a269e72b688b1
ThaiQ <quachhongthai200@gmail.com>
2020-11-20 15:59:23 -0800

    samepl json of tweet

--- search.py
+++ search.py
@@ -4,6 +4,6 @@ import tweepy
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 api = tweepy.API(auth)
 
-tweets = api.search('scu lightning complex wildfire', count = '120')
+tweets = api.search('scu lightning complex wildfire -is:verified -is:retweet', count = '120')
 for tweet in tweets:
   print(tweet.text)
