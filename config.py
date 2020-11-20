a63b9ca976f7e97d602d8a960be5eafc164426af
Seema Vora <seema.s.vora@gmail.com>
2020-11-20 09:24:05 -0800

    Sends Automatted Messages Using Twilio

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 consumer_key = 'DCZk4Ln3ZlrAp6u8g97cLxMqh'
 consumer_secret = 'zJTSFfeUOMTOX6nH8ImWOvc3E9Yu1Rr4eOXa3VtnWHHtMZc4Ze'
 
-twilio_sid = 'SK356e9c2d08df58b3e0f4d7a0baf30cab'
-twilio_auth_token = 'LmyJQWjp8jusrd9UEt66aTBOn8f0MXvt'
+twilio_sid = 'AC4fbedeb9340a6a7ac2745ac44e5a2842'
+twilio_auth_token = '3ebb25284b03e7de1c21dbfd48903d43'
 
-from_phone_number = '+4087074456'
-to_phone_number = '+4087074456'
+from_phone_number = '+12705161184'
+to_phone_number = '+14087074456' 
--- sendMessage.py
+++ sendMessage.py
@@ -11,12 +11,18 @@ def send_message(tweet):
   client = Client(account_sid, auth_token)
 
   message = client.messages \
-    .create{
+    .create(
       body = tweet,
       from_ = config.from_phone_number,
       to = config.to_phone_number
-    }
+    )
   return "Message sent with id{}".format(message.sid)
 
-  
+tweets = api.search('harry styles', count = 10)
+tweet = tweets[0]
+sid = send_message(tweet.text)
+print(sid)
+
+
+
 
