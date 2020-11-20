8202532704f7aa8abdf7fb491e8c571ae28b032b
surabhi gupta <surabhi2029@gmail.com>
2020-11-20 14:10:31 -0800

    Adds fireAPI key to config

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ twilio_auth_token = '404415e800f33e793e9ddd65646af367'
 from_phone_number = '+12705161184'  # from twilio
 to_phone_number = '+14087074456'
 
+fireAPIKey = "1e0a4116590044779598024a9a205866"
+
 # all the keys are on discord PM please check there
