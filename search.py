2a4e973c97a95a009b298c4014ee15769f3c0ea6
Seema Vora <seema.s.vora@gmail.com>
2020-11-20 19:25:44 -0800

    Optimizes Search Feature

--- search.py
+++ search.py
@@ -4,6 +4,15 @@ import tweepy
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 api = tweepy.API(auth)
 
-tweets = api.search('scu lightning complex wildfire -is:verified -is:retweet', count = '120')
+tweets = api.search('pinehaven', count = '23')
+default = 10
+count = 0
+textTweet = []
 for tweet in tweets:
-  print(tweet.text)
+  if (tweet.user.verified == True and count <= 2 ):
+    textTweet.append(tweet.text)
+    count += 1
+  elif(tweet.retweet_count >= default and tweet.retweeted == False):
+      default = tweet.retweet_count
+      textTweet.append(tweet.text)
+print(textTweet)
