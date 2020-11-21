ffdbdf947b020f74bde572f6be3134ea530f30cd
ThaiQ <quachhongthai200@gmail.com>
2020-11-21 15:30:15 -0800

    github pages

--- twitter-tracker/src/index.js
+++ twitter-tracker/src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+
+
+
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact component={App}/>
+    </Switch>
+  </Router>
+)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
